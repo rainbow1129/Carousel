a35cfd85908a60c76012630df6ed5b1a72c08228
Haihong Wang <rainbow.wh@gmail.com>
2015-10-28 00:34:51 -0700

    initial commit

---
+++ Carousel/IntroViewController.swift
@@ -0,0 +1,38 @@
+//
+//  IntroViewController.swift
+//  Carousel
+//
+//  Created by Haihong Wang on 10/28/15.
+//  Copyright © 2015 Haihong Wang. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var introScrollView: UIScrollView!
+    @IBOutlet weak var introImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        introScrollView.contentSize = introImageView.image!.size
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
